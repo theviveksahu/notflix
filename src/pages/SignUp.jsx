c56dede71f40696a2f52f2bd5ad9d5f357024545
Vivek Sahu <viveksahu23@gmail.com>
2024-01-21 15:55:02 +0530

    signup page complete

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
+
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: "Email is required",
+    })
+    .email("Please enter a valid email address"),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
+
 const SignUp = () => {
+  const navigate = useNavigate();
   const [showPassword, setShowpassword] = useState(false);
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  const isLoading = formState.isSubmitting;
+
+  const handleSignIn = async (formData) => {
+    console.log(formData);
+    try {
+      const { email, password } = formData;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) navigate("/");
+  });
 
   return (
     <div className="relative">
@@ -21,24 +66,34 @@ const SignUp = () => {
               membership.
             </h6>
           </div>
-          <div
+          <form
+            id="signup-form"
+            onSubmit={handleSubmit(handleSignIn)}
             className={`w-[50%] grid  gap-2 ${
               showPassword ? "grid-cols-[1fr_1fr]" : "grid-cols-[2fr_1fr]"
             }`}
           >
             <input
-              className="text-white p-4 text-sm border rounded-md border-white bg-[rgba(0,0,0,0.3)] focus:outline-none"
+              {...register("email")}
+              className="text-white p-4 text-sm border rounded-md border-gray-300 bg-[rgba(0,0,0,0.3)] focus:outline-none"
               type="email"
-              name="email"
               placeholder="Email address"
             />
+            {formState.errors.email?.message && (
+              <p>{formState.errors.email?.message}</p>
+            )}
             {showPassword && (
-              <input
-                className="text-white p-4 text-sm border border-white bg-[rgba(0,0,0,0.3)] focus:outline-none rounded-md"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
+              <>
+                <input
+                  {...register("password")}
+                  className="text-white p-4 text-sm border border-gray-300 bg-[rgba(0,0,0,0.3)] focus:outline-none rounded-md"
+                  type="password"
+                  placeholder="Password"
+                />
+                {formState.errors.password?.message && (
+                  <p>{formState.errors.password?.message}</p>
+                )}
+              </>
             )}
 
             {!showPassword && (
@@ -49,9 +104,13 @@ const SignUp = () => {
                 Get Started
               </button>
             )}
-          </div>
-          <button className="py-2 px-4 bg-[#E50914] border-0 cursor-pointer text-white rounded-md font-semibold text-sm hover:bg-opacity-90">
-            Log In
+          </form>
+          <button
+            form="signup-form"
+            disabled={isLoading}
+            className="py-2 px-4 bg-[#E50914] border-0 cursor-pointer text-white rounded-md font-semibold text-sm hover:bg-opacity-90"
+          >
+            Sign Up
           </button>
         </div>
       </div>
